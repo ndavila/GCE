f1f91591e91952866937d620ff38f06d95e70533
Ubuntu <ubuntu@ip-172-31-37-145.ec2.internal>
2018-12-10 15:52:32 +0000

    added graphing to project

--- project/GCE.py
+++ project/GCE.py
@@ -1,21 +1,23 @@
-import Rappture
 import sys
-from math import *
+import Rappture
 import numpy as np
 from scipy.integrate import odeint
 from scipy import optimize
 
-def f(x, formula):
+def f(y, x, formula):
     return eval(formula)
 
-def calc(y, x, formula):
-    return eval(formula)
+def calc(x, a, formula):
+    y = odeint(f, -a, [0,x], args=(formula,))
+    return y[1] 
 
 def main():
+
     io = Rappture.library(sys.argv[1])
 
     xmin = float(io.get('input.number(min).current'))
     xmax = float(io.get('input.number(max).current'))
+    a = float(io.get('input.number(a).current'))
     y0 = float(io.get('input.number(Y0).current'))
     npts = int(io.get('input.number(Npts).current'))
     formula = io.get('input.string(formula).current')
@@ -33,23 +35,33 @@ def main():
 
     for i in range(npts):
         io.put(
-               'output.curve(result0).component.xy',
-               '%g %g\n' % (x[i],f(x[i],formula)), append=1
+                'output.curve(result0).component.xy',
+                '%g %g\n' % (x[i],f(x[i],formula)), append=1
               )
         io.put(
-               'output.curve(result1).component.xy',
-               '%g %g\n' % (x[i],sol[i][0]), append=1
-              )
+                'output.curve(result1).component.xy',
+                '%g %g\n' % (x[i],sol[i][0]), append=1
+              )      
 
-    root = optimize.brentq(f, xmin, xmax, args=(formula,))
+    my_str_base = 'int_0^root (' + formula + ') dx - ' + str(a)
 
-    my_str = 'Root of f(x) in the range ' + str(xmin) + ' to ' + \
-             str(xmax) + ' is ' + str(root)
+    root = optimize.brentq(calc, xmin, xmax, args=(a, formula))
 
-    io.put('output.string(result2).about.label', 'Root')
-    io.put('output.string(result2).current', my_str)
+    my_str = '\nRoot of ' + my_str_base + ' in [' + str(xmin) + \
+            ', ' + str(xmax) + '] is ' + str(root)
+
+    io.put('output.string(result1).about.label', 'Root')
+    io.put('output.string(result1).current', my_str)
+
+    check = calc(root, a, formula)
+
+    my_str2 = '\nCheck: ' + my_str_base + ' = ' + str(check[0])
+
+    io.put('output.string(result2).about.label', 'Diagnostic')
+    io.put('output.string(result2).current', my_str2)
 
     Rappture.result(io)
+
 
 if __name__ == "__main__":
     main()
---
+++ project/project.py
@@ -0,0 +1,85 @@
+import sys
+import Rappture
+import numpy as np
+from scipy.integrate import odeint
+from scipy import optimize
+
+def f(m, t, formula):
+    return eval(formula)
+
+def calc(t, a, formula):
+    m = odeint(f, -a, [0,t], args=(formula,))
+    return m[1]  # y[0] is int m(t) from 0 to 0
+
+def g(t, formula):
+    return eval(formula)
+
+def calc2(y, t, formula):
+    return eval(formula)
+
+def main():
+
+    # Load library--create object
+
+    io = Rappture.library(sys.argv[1])
+
+    # Get input
+
+    tmin = float(io.get('input.number(min).current'))
+    tmax = float(io.get('input.number(max).current'))
+    a = float(io.get('input.number(a).current'))
+    y0 = float(io.get('input.number(Y0).current'))
+    npts = int(io.get('input.number(Npts).current'))
+    formula = io.get('input.string(formula).current')
+
+    t = np.linspace(tmin, tmax, npts)
+    sol = odeint(calc2, y0, t, args=(formula,))
+
+    io.put('output.curve(result2).about.label','M(t) vs t',append=2)
+    io.put('output.curve(result2).yaxis.label','M(t)')
+    io.put('output.curve(result2).xaxis.label','t')
+
+    io.put('output.curve(result3).about.label','Integral of M(t) vs t',append=2)
+    io.put('output.curve(result3).yaxis.label','Integral of M(t)')
+    io.put('output.curve(result3).xaxis.label','t')
+
+    for i in range(npts):
+        io.put(
+                'output.curve(result2).component.xy',
+                '%g %g\n' % (t[i],g(t[i],formula)), append=1
+              )
+        io.put(
+                'output.curve(result3).component.xy'
+                '%g %g\n' % (t[i],sol[i][0]), append=1
+              )
+
+    # Get base string
+
+    my_str_base = 'int_0^root (' + formula + ') dt - ' + str(a)
+
+    # Compute root of \int_0^x f(t') dt' - a
+
+    root = optimize.brentq(calc, tmin, tmax, args=(a, formula))
+
+    my_str = '\nRoot of ' + my_str_base +  ' in [' + str(tmin) + \
+            ', ' + str(tmax) + '] is ' + str(root)
+
+    io.put('output.string(result1).about.label', 'Root')
+    io.put('output.string(result1).current', my_str)
+
+    # Check
+
+    check = calc(root, a, formula)
+
+    my_str2 = '\nCheck: ' + my_str_base + ' = ' + str(check[0])
+
+    io.put('output.string(result2).about.label', 'Diagnostic')
+    io.put('output.string(result2).current', my_str2)
+
+    # Output results
+
+    Rappture.result(io)
+
+
+if __name__ == "__main__":
+    main()
