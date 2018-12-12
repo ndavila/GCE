542f8f1b23e218c3deb24bc61e41c271f9492973
Ubuntu <ubuntu@ip-172-31-37-145.ec2.internal>
2018-12-12 18:07:42 +0000

    still trying to get root to work

--- project/project.py
+++ project/project.py
@@ -5,13 +5,6 @@ from scipy.integrate import odeint
 from scipy import optimize
 from math import *
 
-def ff(m, t, formula):
-    return eval(formula)
-
-def calc(t, a, formula):
-    m = odeint(f, -a, [0,t], args=(formula,))
-    return m[1]  # y[0] is int m(t) from 0 to 0
-
 def f(t, formula):
     return eval(formula)
 
@@ -20,6 +13,10 @@ def total_mass(y, t, formula):
 
 def gas_mass(y, t, formula, omega):
     return -omega * y[0] + f(t, formula)
+
+def calc(y0, t, formula, gas_mass, omega):
+    x = odeint(gas_mass[0], y0, t, args = (formula,omega))
+    return x[1] - gas_mass[0]
 
 def main():
 
@@ -31,58 +28,49 @@ def main():
 
     tmin = float(io.get('input.number(min).current'))
     tmax = float(io.get('input.number(max).current'))
-    a = float(io.get('input.number(a).current'))
+#    a = float(io.get('input.number(a).current'))
     y0 = float(io.get('input.number(Y0).current'))
     npts = int(io.get('input.number(Npts).current'))
     formula = io.get('input.string(formula).current')
 
     t = np.linspace(tmin, tmax, npts)
+
     omega = 0.3
+
     y = odeint(gas_mass, y0, t, args=(formula,omega))
-#    y = odeint(total_mass, y0, t, args=(formula,))
+    
+#    z = odeint(total_mass, y0, t, args=(formula,))
 
-    io.put('output.curve(result2).about.label','f(t) vs t',append=0)
-    io.put('output.curve(result2).yaxis.label','f(t)')
+    io.put('output.curve(result0).about.label','f(t) vs t',append=0)
+    io.put('output.curve(result0).yaxis.label','f(t)')
+    io.put('output.curve(result0).xaxis.label','t')
+
+    io.put('output.curve(result2).about.label',
+            'Integral of dMG/dt vs t',append=0)
+    io.put('output.curve(result2).yaxis.label',
+            'Integral of dMG/dt')
     io.put('output.curve(result2).xaxis.label','t')
-
-    io.put('output.curve(result4).about.label','Integral of f(t) vs t',append=0)
-    io.put('output.curve(result4).yaxis.label','Integral of f(t)')
-    io.put('output.curve(result4).xaxis.label','t')
 
     for i in range(npts):
         io.put(
-                'output.curve(result2).component.xy',
+                'output.curve(result0).component.xy',
                 '%g %g\n' % (t[i],f(t[i],formula)), append=1
               )
         io.put(
-                'output.curve(result4).component.xy',
+                'output.curve(result2).component.xy',
                 '%g %g\n' % (t[i],y[i][0]), append=1
               )
 
-    # Get base string
+#    root = optimize.brentq(calc, y0, tmin, tmax,
+#            args =  (omega, formula, gas_mass))
 
-#    my_str_base = 'int_0^root (' + formula + ') dt - ' + str(a)
+    my_str_base = 'int_0^root (' + formula + ')dx '
 
-    # Compute root of \int_0^x f(t') dt' - a
+    my_str = '\nRoot of ' + my_str_base + '' 
 
-#    root = optimize.brentq(calc, tmin, tmax, args=(a, formula))
+    io.put('output.string(result1).about.label', 'Root')
+    io.put('output.string(result1).current', my_str)
 
-#    my_str = '\nRoot of ' + my_str_base +  ' in [' + str(tmin) + \
-#            ', ' + str(tmax) + '] is ' + str(root)
-
-#    io.put('output.string(result1).about.label', 'Root')
-#    io.put('output.string(result1).current', my_str)
-
-    # Check
-
-#    check = calc(root, a, formula)
-
-#    my_str2 = '\nCheck: ' + my_str_base + ' = ' + str(check[0])
-
-#    io.put('output.string(result2).about.label', 'Diagnostic')
-#    io.put('output.string(result2).current', my_str2)
-
-    # Output results
 
     Rappture.result(io)
 
